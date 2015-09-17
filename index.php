da8386d617ae4686bb423717be71e2f4e0e1cf8c
smit9960 <jsmithwoodworm@yahoo.com>
2015-09-17 11:22:39 -0700

    third commit

--- index.php
+++ index.php
@@ -1,162 +1,21 @@
-<?php 
+ 
+<?php
 
-// $prices = array();
-// $prices = array(550, 200, 600);
-// 
-// echo $prices[0];
-// 
-// $prices[] = 100;
-// 
-// array_push($prices, 200);
-// 
-// print_r($prices);
-// 
-// for($i = 0; $i < count($prices); $i++)
-// {
-	// echo $prices;
-	// echo "<br />";
-// }
-// 
-// // Associative Array
-// 
-// $prices = array("Apple" => 500, "iPad Mini" => 250);
-// 
-// foreach ($prices as $value) {
-// 	
-	// echo $value . "<br />";
-// }
-// 
-// asort($prices);
-// ksort($prices);
+$deck = array();
 
-
-// 
-// $userList = array("Adam"=>0, 
-// 
-// "Peter"=> 0, 
-// 
-// "Stewie"=>0, 
-// 
-// "Meg"=>0, 
-// 
-// "Joe"=>0, 
-// 
-// "Bonny"=>0, 
-// 
-// "Quacmire"=>0, 
-// 
-// "Bryan"=>0, 
-// 
-// "Tom"=>0, 
-// 
-// "Cleveland"=>0, 
-// 
-// );
-// 
-// foreach ($userList as $key => $value) {
-// 	
-	// $key = $value = genPass();
-// }
-// 
-// print_r($userList);
-// 
-// 
-// 
-// function genPass(){
- // $password = array();
-//  
- // for($i = 0; $i < 7; $i++)
- // {
- 	// $password[$i] = chr(rand(65, 90));
- // }
-//  
- // $password[rand(0, count($password))] = rand(0, 9);
-//  
- // <?php 
-
-// $prices = array();
-// $prices = array(550, 200, 600);
-// 
-// echo $prices[0];
-// 
-// $prices[] = 100;
-// 
-// array_push($prices, 200);
-// 
-// print_r($prices);
-// 
-// for($i = 0; $i < count($prices); $i++)
-// {
-	// echo $prices;
-	// echo "<br />";
-// }
-// 
-// // Associative Array
-// 
-// $prices = array("Apple" => 500, "iPad Mini" => 250);
-// 
-// foreach ($prices as $value) {
-// 	
-	// echo $value . "<br />";
-// }
-// 
-// asort($prices);
-// ksort($prices);
-// 
-// 
-// 
-// $userList = array("Adam"=>0, 
-// 
-// "Peter"=> 0, 
-// 
-// "Stewie"=>0, 
-// 
-// "Meg"=>0, 
-// 
-// "Joe"=>0, 
-// 
-// "Bonny"=>0, 
-// 
-// "Quacmire"=>0, 
-// 
-// "Bryan"=>0, 
-// 
-// "Tom"=>0, 
-// 
-// "Cleveland"=>0, 
-// 
-// );
-// 
-// foreach ($userList as $key => $value) {
-// 	
-	// $key = $value = genPass();
-// }
-// 
-// print_r($userList);
-// 
-// 
-// 
-// function genPass(){
- // $password = array();
-//  
- // for($i = 0; $i < 7; $i++)
- // {
- 	// $password[$i] = chr(rand(65, 90));
- // }
-//  
- // $password[rand(0, count($password))] = rand(0, 9);
-//  
- // return $password;
-// }
-//  
-//  
-//  
-//  
-
-?>
+for ($i = 0; $i < 52; $i++){
+	$deck[] = $i;
 }
- 
- 
- 
- 
+
+shuffle($deck);
+print_r($deck);
+
+echo "<br ?>";
+echo "<br ?>";
+$lastCard = array_pop($deck);
+$suitArray = array("clubs", "diamonds", "hearts", "spades");
+
+echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/1.png' / >";
+echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/" . ($lastCard % 13 + 1) . ".png' / >";
+
 ?>
