a0752697ff807ddebfbaf158316fea3f5d5e005a
snyd4924 <rsnyder@csumb.edu>
2015-09-21 11:37:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
- 
 <?php
 
 $deck = array();
@@ -8,14 +7,20 @@ for ($i = 0; $i < 52; $i++){
 }
 
 shuffle($deck);
-print_r($deck);
 
 echo "<br ?>";
 echo "<br ?>";
-$lastCard = array_pop($deck);
 $suitArray = array("clubs", "diamonds", "hearts", "spades");
 
-echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/1.png' / >";
-echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/" . ($lastCard % 13 + 1) . ".png' / >";
+//echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/1.png' / >";
+
+for($i = 0; $i < 4; $i++){
+
+	for ($j = 0; $j < rand(4, 6); $j++){
+		echo "<img src ='cards/" . $suitArray[floor(array_pop($deck)/13)] . "/" . (array_pop($deck) % 13 + 1) . ".png' / >";	
+	}
+
+echo "<br />";
+}
 
 ?>
