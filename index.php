2c54968d67fc41f7a3e92899db6a9c5bd187a83f
snyd4924 <rsnyder@csumb.edu>
2015-09-21 15:55:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
-
 $deck = array();
-
 for ($i = 0; $i < 52; $i++){
 	$deck[] = $i;
 }
-
 shuffle($deck);
-
 echo "<br ?>";
 echo "<br ?>";
 $suitArray = array("clubs", "diamonds", "hearts", "spades");
-
 //echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/1.png' / >";
  $theCardValues = array("1.png" => 1,
                     "2.png" => 2,
@@ -23,16 +18,18 @@ $suitArray = array("clubs", "diamonds", "hearts", "spades");
                     "8.png" => 8, 
                     "9.png" => 9, 
                     "10.png" => 10, 
-                    "11.png" => 10, 
-                    "12.png" => 10, 
-                    "13.png" => 10,);
+                    "11.png" => 11, 
+                    "12.png" => 12, 
+                    "13.png" => 13,);
+
  
 for($i = 0; $i < 4; $i++){
-
+	$sum = 0;
 	for ($j = 0; $j < rand(4, 6); $j++){
 		echo "<img src ='cards/" . $suitArray[floor(array_pop($deck)/13)] . "/" . (array_pop($deck) % 13 + 1) . ".png' / >";	
+		$sum = $sum + $theCardValues[(array_pop($deck) % 13 + 1) . ".png"];
 	}
-
+	echo $sum;
 echo "<br />";
 }
 
