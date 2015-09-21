b2325b7d1870f9a884023bfa2cc37c8899952a8d
smit9960 <jsmithwoodworm@yahoo.com>
2015-09-21 15:32:14 -0700

    added card value array

--- index.php
+++ index.php
@@ -13,7 +13,20 @@ echo "<br ?>";
 $suitArray = array("clubs", "diamonds", "hearts", "spades");
 
 //echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/1.png' / >";
-
+ $theCardValues = array("1.png" => 1,
+                    "2.png" => 2,
+                    "3.png" => 3,
+                    "4.png" => 4,
+                    "5.png" => 5,
+                    "6.png" => 6, 
+                    "7.png" => 7, 
+                    "8.png" => 8, 
+                    "9.png" => 9, 
+                    "10.png" => 10, 
+                    "11.png" => 10, 
+                    "12.png" => 10, 
+                    "13.png" => 10,);
+ 
 for($i = 0; $i < 4; $i++){
 
 	for ($j = 0; $j < rand(4, 6); $j++){
