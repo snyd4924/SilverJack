787de3009d8a2f90a3fba10af0ebc62111a7fc76
snyd4924 <rsnyder@csumb.edu>
2015-09-21 18:03:29 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,91 @@
 <?php
+
 $deck = array();
+
 for ($i = 0; $i < 52; $i++){
-	$deck[] = $i;
+
+$deck[] = $i;
+
 }
 
 echo "<br ?>";
+
 echo "<br ?>";
+
 $suitArray = array("clubs", "diamonds", "hearts", "spades");
+
 //echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/1.png' / >";
+
  $theCardValues = array("1.png" => 1,
+
                     "2.png" => 2,
+
                     "3.png" => 3,
+
                     "4.png" => 4,
+
                     "5.png" => 5,
+
                     "6.png" => 6, 
+
                     "7.png" => 7, 
+
                     "8.png" => 8, 
+
                     "9.png" => 9, 
+
                     "10.png" => 10, 
+
                     "11.png" => 11, 
+
                     "12.png" => 12, 
+
                     "13.png" => 13,);
 
 shuffle($deck);
+
  $sum = array();
+
 for($i = 0; $i < 4; $i++){
+
 $sum[$i] = 0;
-	for ($j = 0; $j < rand(4, 6); $j++){
-		echo "<img src ='cards/" . $suitArray[floor(array_pop($deck)/13)] . "/" . (array_pop($deck) % 13 + 1) . ".png' / >";		
-		$sum[$i] = $sum[$i] + $theCardValues[(array_pop($deck) % 13 + 1) . ".png"];
-	}
-	echo $sum[$i] . " ";
-	
-	if ($sum[$i] == 42){	
-		echo "YOU WIN! ";	
-	}
-	
+
+for ($j = 0; $j < rand(4, 6); $j++){
+
+$lastCard =  array_pop($deck);
+
+$cardToDisplay = ($lastCard % 13) + 1;
+
+echo "<img src ='cards/" . $suitArray[floor($lastCard/13)] . "/" . $cardToDisplay . ".png' / >";
+
+$sum[$i] = $sum[$i] + $theCardValues[$cardToDisplay. ".png"];
+
+}
+
+echo $sum[$i] . " ";
+
+
+if ($sum[$i] == 42){
+
+echo "YOU WIN! ";
+
+}
+
+
 echo "<br />";
+
 }
 
 for ($i = 3; $i > 0; $i--){
-	$temp = $sum[$i];
-	if ($sum[$i] > $sum[$i-1] && $sum[$i] < 42){
-		$temp = $sum[$i];	
-	}
+
+$temp = $sum[$i];
+
+if ($sum[$i] > $sum[$i-1] && $sum[$i] < 42){
+
+$temp = $sum[$i];
+
 }
+
+}
+
 ?>
